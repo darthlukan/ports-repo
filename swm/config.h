e364baf463d0e8855b58d2b9326c2d20ae33bab9
Brian Tomlinson <darthlukan@gmail.com>
2015-10-04 15:22:11 +0200

    update i3 md5sum, update swm with my config

---
+++ swm/config.h
@@ -0,0 +1,14 @@
+#define MOD         SUPER
+
+/* INNER + OUTER should not be greater than BORDERWIDTH */
+#define BORDERWIDTH 2
+#define INNER       1
+#define OUTER       1
+
+/* colors */
+#define FOCUSCOL    0xEAEAEA
+#define UNFOCUSCOL  0X333333
+#define OUTERCOL    0x000000
+
+#define ENABLE_MOUSE
+#define DOUBLE_BORDER
